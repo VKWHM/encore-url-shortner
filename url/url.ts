ecf3b06e6f70f0e90d97f39c34febeabe6ed5fac
VKWHM <111775089+VKWHM@users.noreply.github.com>
2024-09-10 10:40:07 +0300

    Integrate Database
    Create first database migration.
    Implement database connection to api endpoints.

--- url/url.ts
+++ url/url.ts
@@ -1,5 +1,8 @@
 import { api } from "encore.dev/api";
+import { SQLDatabase } from "encore.dev/storage/sqldb";
 import { randomBytes } from "node:crypto";
+
+const db = new SQLDatabase("urls_shortner", { migrations: "./migrations" });
 
 interface ShortnerRequest {
   url: string;
@@ -17,6 +20,7 @@ export const shortner = api<ShortnerRequest, ShortnerResponse>(
   },
   async ({ url }) => {
     const shortid = randomBytes(4).toString("base64url");
+    await db.exec`INSERT INTO urls (id, url) VALUES (${shortid}, ${url});`;
     return { shortid: `/${shortid}` };
   },
 );
@@ -32,6 +36,7 @@ interface LongnerResponse {
 export const longner = api<LongnerRequest, LongnerResponse>(
   { path: "/:shortid", method: "GET", expose: true },
   async ({ shortid }) => {
-    return { url: shortid };
+    const row = await db.queryRow`SELECT url FROM urls WHERE id = ${shortid}`;
+    return { url: row?.url };
   },
 );
